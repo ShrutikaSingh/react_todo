21be9c6788fcca41f2fa4f0159855a66b9f7035a
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-28 19:41:42 +0530

    update ListItem with delete task

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import './ListItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function ListItems(props){
   const items=props.items;
-  console.log(items)
-  const listItems=items.map(item=>{
-    return <div key='item.key'>
-    <p>  {item.text} </p>
+  const listItems=items.map(item=>
+    {
+    return <div className="list" key={item.key}>
+    <p>  {item.text}
+    <span>
+      <FontAwesomeIcon className="faicons" icon="trash" onClick={()=>{
+        props.deleteItem(item.key)
+      }} icon="trash"/>
+      </span>
+    </p>
     </div>
+
   })
     return(
       <div>
